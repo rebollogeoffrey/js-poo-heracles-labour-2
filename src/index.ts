9e94c0b7038894e59b4fa5f0514d5cfbbe3d7c4c
rebollogeoffrey <geoffrey.rebollo@free.fr>
2023-07-26 10:57:05 +0200

    change comment

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ const Lion: Fighter = new Fighter("\u{1F981} Nemean Lion", 11, 13);
 const Sanglier: Fighter = new Fighter("🐗 Boar of Erymanthe", 20, 25);
 
 // WEAPONS
-const Stick: Weapon = new Weapon("Pointy stick");
+const Stick: Weapon = new Weapon("The Stick ?!"); // Tribute to the best's anecdoctes group ever
 const Paper: Weapon = new Weapon("Paper"); // Fear the paper cut!
 const Bondage: Weapon = new Weapon("Bondage", -10); //Fear the heal // Not only moral support
 const Sword: Weapon = new Weapon("Épée");
@@ -24,11 +24,10 @@ const HotPepper: Weapon = new Weapon("Hot pepper", 100); //Burn body, spirit and
 const Napoleon: Weapon = new Weapon("Three centimeters", 100); // Small range but huge damage
 
 //SHIELDS
-const Stack: Shield = new Shield("The Stack !?");
+const Stack: Shield = new Shield("The Stack !?"); // Tribute to the best's anecdoctes group ever
 const TheLook: Shield = new Shield("The Look");
 const Wall: Shield = new Shield("Brick's wall");
 const Hylian: Shield = new Shield("Hylian Shield");
-
 
 const displayTxtIntro = (Monster: Fighter) => {
   console.log(
@@ -39,43 +38,53 @@ const displayTxtIntro = (Monster: Fighter) => {
     Monster.name,
     "is there, annoyed by his unexpected guest who did not knock before entering his lair. He keeps his red eyes fixed on his new enemy.\nA fight is imminent:"
   );
-}
+};
 
-const setWeaponAndShieldToFighter = (fighter: Fighter,weapon? : Weapon, shield?: Shield) => {
+const setWeaponAndShieldToFighter = (
+  fighter: Fighter,
+  weapon?: Weapon,
+  shield?: Shield
+) => {
   let text: string = fighter.name;
 
   if (weapon != null) {
     fighter.setWeapon(weapon);
-    text += " uses " + weapon.name, "to attack";
+    (text += " uses " + weapon.name), "to attack";
   }
-  if (shield!= null && text != fighter.name ) {
+  if (shield != null && text != fighter.name) {
     text += " and ";
   }
-  if (shield!= null) {
+  if (shield != null) {
     fighter.setShield(shield);
-  text += " with " + shield.name, "to defend";
+    (text += " with " + shield.name), "to defend";
   }
-}
+};
 
 const displayTxtOutroWinner = (Winner: Fighter) => {
-    console.warn(
-      "\u{21DB}🏆",
-      Winner.name,
-      "wins with",
-      Winner.life + " point(s) of life"
-    );
+  console.warn(
+    "\u{21DB}🏆",
+    Winner.name,
+    "wins with",
+    Winner.life + " point(s) of life"
+  );
 };
 
 const displayTxtOutroLoser = (Loser: Fighter) => {
-  console.warn(
-    "\n💀 ", Loser.name, "is dead \u{1FAA6}"
-  );
-}
+  console.warn("\n💀 ", Loser.name, "is dead \u{1FAA6}");
+};
 
 // Start a fight against the Lion
-const labourBattle = (Monster: Fighter, usedWeapon? : Weapon, usedShield? : Shield) => {
+const labourBattle = (
+  Monster: Fighter,
+  usedWeapon?: Weapon,
+  usedShield?: Shield
+) => {
   displayTxtIntro(Monster);
-  setWeaponAndShieldToFighter(Human, usedWeapon? usedWeapon : Sword, usedShield? usedShield : TheLook);
+  setWeaponAndShieldToFighter(
+    Human,
+    usedWeapon ? usedWeapon : Sword,
+    usedShield ? usedShield : TheLook
+  );
 
   let round: number = 0;
   while (Human.isAlive() && Monster.isAlive()) {
@@ -89,14 +98,17 @@ const labourBattle = (Monster: Fighter, usedWeapon? : Weapon, usedShield? : Shie
   }
 
   if (Monster.life === 0) {
-    displayTxtOutroLoser(Monster)
+    displayTxtOutroLoser(Monster);
     displayTxtOutroWinner(Human);
     console.warn(
       "\n\u{26A0}",
       Human.name +
         " walks away from the battle when he steps on a pebble on the road..."
     );
-    console.log(Pebble.name + " is very angry seing his nemesis staying alive. It's strengh has been levelling up each round since the beginning of the fight.");
+    console.log(
+      Pebble.name +
+        " is very angry seing his nemesis staying alive. It's strengh has been levelling up each round since the beginning of the fight."
+    );
     Pebble.setStrengthValue(round);
     Pebble.fight(Human);
     if (Human.life === 0) {
@@ -114,19 +126,25 @@ const labourBattle = (Monster: Fighter, usedWeapon? : Weapon, usedShield? : Shie
   }
 };
 
-// labourBattle(Lion);
 const fighterName = process.argv[2];
 const fighterStrength = parseInt(process.argv[3]);
 const fighterDexterity = parseInt(process.argv[4]);
 
-const fighterFromCLI:Fighter = new Fighter(fighterName, fighterStrength, fighterDexterity)
+const fighterFromCLI: Fighter = new Fighter(
+  fighterName,
+  fighterStrength,
+  fighterDexterity
+);
 
-const defaultBattle = () =>{
+const defaultBattle = () => {
   const random = Math.floor(Math.random() * 5);
-  console.log('random :>> ', random);
-  if(random<=2) return Lion;
+  console.log("random :>> ", random);
+  if (random <= 2) return Lion;
   else return Sanglier;
-}
+};
 
-
-labourBattle(process.argv[2]!==undefined? fighterFromCLI : defaultBattle(), Stick, Stack);
+labourBattle(
+  process.argv[2] !== undefined ? fighterFromCLI : defaultBattle(),
+  Stick,
+  Stack
+);
